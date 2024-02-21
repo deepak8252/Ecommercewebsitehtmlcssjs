1bd541a4f845a4f4c4f428c3edf2eed7aca7ef8d
Deepak kumar <deepak.kumar.82521@gmail.com>
2024-02-21 22:39:43 +0530

    first commit

---
+++ assest/js/index.js
@@ -0,0 +1,13 @@
+const bar=document.getElementById("bar");
+const closebar=document.getElementById("close");
+const nav=document.getElementById("navbar");
+if(bar){
+    bar.addEventListener("click",()=>{
+        nav.classList.add("active");
+    })
+}
+if(closebar){
+  closebar.addEventListener("click",()=>{
+    nav.classList.remove("active");
+  })
+}
